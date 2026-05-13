f6a8afc896c240e81e4b9c08b6044b1f25ccbb42
Rangerboy574 <jdeweese574@icloud.com>
2026-05-13 18:11:19 -0500

    Initial Commit

---
+++ Ordem/OrdemApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrdemApp.swift
+//  Ordem
+//
+//  Created by Joseph DeWeese on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct OrdemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
